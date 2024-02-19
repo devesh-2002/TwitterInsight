bbb215ac2ac353202ee4c8f657ab42ee1203e9fb
Devesh R <dev241202@gmail.com>
2024-02-19 16:16:27 +0530

    Initialized Next Frontend

--- devrev-snapin/code/src/function-factory.ts
+++ devrev-snapin/code/src/function-factory.ts
@@ -1,8 +1,9 @@
 import on_work_creation from './functions/on_work_creation';
-
+import twitter from './functions/twitter';
 export const functionFactory = {
   // Add your functions here
   on_work_creation,
+  twitter
 } as const;
 
 export type FunctionFactoryType = keyof typeof functionFactory;
